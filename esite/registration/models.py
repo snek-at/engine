5c7d492afc3ed144ccd5a24aae3352342225f2af
kleberbaum <kleberbaum@erebos.xyz>
2020-08-13 05:35:44 +0200

    adadadada

--- esite/registration/models.py
+++ esite/registration/models.py
@@ -617,7 +617,7 @@ class EnterpriseRegistrationFormPage(BaseEmailFormPage):
     def process_form_submission(self, form):
 
         user = self.create_user(
-            enterprise_username=form.cleaned_data["enterprise_name"],
+            enterprise_username=form.cleaned_data["enterprise_username"],
             city=form.cleaned_data["city"],
             zip_code=form.cleaned_data["zip_code"],
             address=form.cleaned_data["address"],
