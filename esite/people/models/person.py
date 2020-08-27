b0f7a1ee15fc40d7f8ca8f2e585524c7bac3024b
schettn <nicoschett@icloud.com>
2020-08-27 20:24:32 +0200

    Add missing graphql fields
    All needed graphql fields for the person model and
    its blocks has been added.

--- esite/people/models/person.py
+++ esite/people/models/person.py
@@ -14,6 +14,8 @@ from wagtail.core.fields import StreamField
 from esite.bifrost.helpers import register_streamfield_block, register_query_field
 from esite.bifrost.models import (
     GraphQLString,
+    GraphQLInt,
+    GraphQLFloat,
     GraphQLStreamfield,
     GraphQLForeignKey,
     GraphQLEmbed,
@@ -27,6 +29,13 @@ class _Person_Language(blocks.StructBlock):
     name = blocks.CharBlock(max_length=255)
     size = blocks.IntegerBlock()
     share = blocks.FloatBlock()
+
+    graphql_fields = [
+        GraphQLString("color"),
+        GraphQLString("name"),
+        GraphQLInt("size"),
+        GraphQLFloat("share"),
+    ]
 
 
 @register_streamfield_block
@@ -53,26 +62,28 @@ class _Person_Project(blocks.StructBlock):
     )
     url = blocks.URLBlock(help_text="Important! Format https://www.domain.tld/xyz")
     name = blocks.CharBlock(max_length=255)
+    fullname = blocks.CharBlock(max_length=255)
     owner = _Person_Member()
     members = blocks.ListBlock(_Person_Member())
-    # languages = blocks.ListBlock(_Person_Language())
+    languages = blocks.ListBlock(_Person_Language())
 
     graphql_fields = [
         GraphQLString("avatar_url"),
         GraphQLString("url"),
         GraphQLString("name"),
+        GraphQLString("fullname"),
         GraphQLString("owner_avatar_url"),
         GraphQLString("owner_url"),
         GraphQLString("owner_fullname"),
         GraphQLString("owner_name"),
         GraphQLStreamfield("owner", is_list=False),
         GraphQLStreamfield("members"),
-        # GraphQLStreamfield("languages"),
+        GraphQLStreamfield("languages"),
     ]
 
 
 @register_streamfield_block
-class _Person_Organization(blocks.StructBlock):
+class _Person_Organisation(blocks.StructBlock):
     profile_name = blocks.CharBlock(max_length=255)
     avatar_url = blocks.URLBlock(
         help_text="Important! Format https://www.domain.tld/xyz"
@@ -80,31 +91,42 @@ class _Person_Organization(blocks.StructBlock):
     url = blocks.URLBlock(help_text="Important! Format https://www.domain.tld/xyz")
     name = blocks.CharBlock(max_length=255)
     fullname = blocks.CharBlock(max_length=255)
-    description = blocks.TextBlock(blank=True)
+    description = blocks.TextBlock(null=True, blank=True)
     members = blocks.ListBlock(_Person_Member())
-    # projects = blocks.ListBlock(_Person_Project())
+    projects = blocks.ListBlock(_Person_Project())
+
+    graphql_fields = [
+        GraphQLString("profile_name"),
+        GraphQLString("avatar_url"),
+        GraphQLString("url"),
+        GraphQLString("name"),
+        GraphQLString("fullname"),
+        GraphQLString("description"),
+        GraphQLStreamfield("members"),
+        GraphQLStreamfield("projects"),
+    ]
 
 
 class Person(ClusterableModel):
     user = ParentalKey("user.SNEKUser", on_delete=models.CASCADE, related_name="person")
 
     projects = StreamField([("project", _Person_Project())], null=True, blank=True)
-    organizations = StreamField(
-        [("organization", _Person_Organization())], null=True, blank=True
+    organisations = StreamField(
+        [("organisation", _Person_Organisation())], null=True, blank=True
     )
 
     # Panels/fields to fill in the Add enterprise form
     panels = [
         FieldPanel("user"),
         MultiFieldPanel(
-            [StreamFieldPanel("projects"), StreamFieldPanel("organizations")]
+            [StreamFieldPanel("projects"), StreamFieldPanel("organisations")]
         ),
     ]
 
     graphql_fields = [
         # GraphQLString("user"),
         GraphQLStreamfield("projects"),
-        GraphQLStreamfield("organizations"),
+        GraphQLStreamfield("organisations"),
     ]
 
     def __str__(self):
